991cce3c330e783a11bd8e41c5b32dcd8da71f96
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-05 22:22:27 +0800

    React Heroes Test App.

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,38 @@
+import { useState, useRef, useEffect } from 'react'
+import type { Hero } from '../types/hero';
+import { Link } from 'react-router-dom';
+import { useMessages } from '../context/MessageContext';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export default function Dashboard() {
+    const [heroes, setHeroes] = useState<Hero[]>([]);
+    const fetched = useRef(false);
+    const {addMessage} = useMessages()
+
+    useEffect(() => {
+        if(!fetched.current) {
+            fetch(`${apiUrl}/heroes?_limit=4`).then(res => {
+                return res.json();
+            }).then(data => {
+                setHeroes(data);
+                addMessage('Top heroes loaded');
+            })
+            fetched.current = true
+        }
+    }, [addMessage])
+    
+    return (
+        <div className='flex flex-col gap 3'>
+            <h2>Top Heroes</h2>
+            <div className='flex gap-3'>
+                {heroes.map( hero => (
+                    <Link key={hero.id} to={`/heroes/${hero.id}`} 
+                    className='p-4 bg-slate-700 text-white rounded-lg cursor-pointer'>
+                        {hero.name}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ src/context/MessageContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useCallback, useContext, useState, type ReactNode } from "react";
+
+type MessageContextType = {
+    messages: string[];
+    addMessage: (message: string) => void;
+    clearMessages: () => void;
+}
+
+const MessageContext = createContext<MessageContextType | undefined>(undefined);
+
+const MessageProvider = ({children}: {children: ReactNode}) => {
+   const [messages, setMessages] = useState<string[]>([]);
+   
+   const addMessage = useCallback((message: string) => {
+    setMessages(prevMessages => [...prevMessages, message]);
+   }, [])
+
+   const clearMessages = () => {
+    setMessages([]);
+   }
+
+   return (
+    <MessageContext.Provider value={{messages, addMessage, clearMessages}}>
+        {children}
+    </MessageContext.Provider>
+   )
+}
+
+const useMessages = () => {
+    const context = useContext(MessageContext);
+    if (context === undefined) {
+        throw new Error("useMessageContext must be used within a MessageProvider");
+    }
+
+    return context;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export {MessageProvider, useMessages};
